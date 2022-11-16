d869a76016abab72dcc6ac6b12175d85fc7650b4
GoneSahlin <zach@sahlins.net>
2022-11-16 03:59:24 -0800

    added startSelectPitch and result listener

--- app/src/main/java/com/example/baseballtracker/SelectPitch.kt
+++ app/src/main/java/com/example/baseballtracker/SelectPitch.kt
@@ -3,6 +3,9 @@ package com.example.baseballtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+const val PITCH_TYPE = "com.example.baseballtracker.pitch_type"
+
 class SelectPitch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/baseballtracker/playgame.kt
+++ app/src/main/java/com/example/baseballtracker/playgame.kt
@@ -1,11 +1,66 @@
 package com.example.baseballtracker
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
 
 class playgame : AppCompatActivity() {
+    private var pitchTypeIndex: Int = -1
+    private lateinit var strikeButton: Button
+    private lateinit var ballButton: Button
+    private lateinit var inPlayButton: Button
+    private lateinit var strikeArray: Array<Int>
+    private lateinit var ballArray: Array<Int>
+    private lateinit var totalArray: Array<Int>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playgame)
+
+        strikeButton = findViewById(R.id.strike_button)
+        ballButton = findViewById(R.id.ball_button)
+        inPlayButton = findViewById(R.id.in_play_button)
+
+        strikeButton.setOnClickListener { onStrikeButtonClicked() }
+        ballButton.setOnClickListener { onBallButtonClicked() }
+        inPlayButton.setOnClickListener { onInPlayButtonClicked() }
+    }
+
+    private fun onStrikeButtonClicked() {
+        startSelectPitch()
+
+        if (pitchTypeIndex != -1) {
+
+        }
+    }
+
+    private fun onBallButtonClicked() {
+
+    }
+
+    private fun onInPlayButtonClicked() {
+
+    }
+
+    private fun startSelectPitch() {
+        val intent = Intent(this, SelectPitch::class.java)
+        selectPitchResultLauncher.launch(intent)
+    }
+
+    val selectPitchResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // Create the "on" button color based on the chosen color ID from ColorActivity
+            pitchTypeIndex = result.data!!.getIntExtra(PITCH_TYPE, 0)
+//            pitchType = resources.getStringArray(R.array.pitch_types)[pitchTypeIndex]
+        } else {
+            pitchTypeIndex = -1
+        }
     }
 }
