e9e139d479faec337f61e204610975a5fd606988
GoneSahlin <zach@sahlins.net>
2022-11-16 02:59:59 -0800

    added select pitch activity

---
+++ app/src/main/java/com/example/baseballtracker/SelectPitch.kt
@@ -0,0 +1,11 @@
+package com.example.baseballtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectPitch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_pitch)
+    }
+}
