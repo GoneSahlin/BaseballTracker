c97357ab86ad5869f07dcf19ab1280e465749451
cbarker4 <cbarker4@zagmail.gonzaga.edu>
2022-11-15 22:55:07 -0800

    Added screens to prevent merge conflicts

--- app/src/main/java/com/example/baseballtracker/MainActivity.kt
+++ app/src/main/java/com/example/baseballtracker/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.baseballtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val  practice = findViewById<Button>(R.id.practice)
+        practice.setOnClickListener{
+            val intent = Intent(this,PracticeMain::class.java)
+            startActivity(intent)
+        }
+        val  play = findViewById<Button>(R.id.playGame)
+        play.setOnClickListener{
+            val intent = Intent(this,playgame::class.java)
+            startActivity(intent)
+        }
+        val  stats = findViewById<Button>(R.id.stats)
+        stats.setOnClickListener{
+            val intent = Intent(this,Stats::class.java)
+            startActivity(intent)
+        }
+        val  settings = findViewById<Button>(R.id.settings)
+        settings.setOnClickListener{
+            val intent = Intent(this,MainSettings::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/baseballtracker/PracticeMain.kt
+++ app/src/main/java/com/example/baseballtracker/PracticeMain.kt
@@ -1,4 +1,27 @@
 package com.example.baseballtracker
 
-class PracticeMain {
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class PracticeMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice_main)
+
+        val  pitching = findViewById<Button>(R.id.Pitching)
+        pitching.setOnClickListener{
+            val intent = Intent(this,Pitching::class.java)
+            startActivity(intent)
+        }
+
+        val  bat = findViewById<Button>(R.id.Batting)
+        bat.setOnClickListener{
+            val intent = Intent(this,Batting::class.java)
+            startActivity(intent)
+
+        }
+    }
 }
