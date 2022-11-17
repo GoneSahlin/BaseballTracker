24fe8720c38be4a879025c0c4b62a67cf2f47bcb
cbarker4 <cbarker4@zagmail.gonzaga.edu>
2022-11-16 20:48:16 -0800

    Piching practice set up

--- app/src/main/java/com/example/baseballtracker/Pitching.kt
+++ app/src/main/java/com/example/baseballtracker/Pitching.kt
@@ -2,10 +2,51 @@ package com.example.baseballtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Pitching : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pitching)
+
+        var spinner = findViewById<Spinner>(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.pitch_types,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(parent: AdapterView<>?) {
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<>?, view: View?, position: Int, id: Long) {
+//                val spinnerSelected = position
+//            }
+
+
+        var pitcher = findViewById<Spinner>(R.id.pitcher)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.pitchers,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            pitcher.adapter = adapter
+        }
+
+//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(parent: AdapterView<>?) {
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<>?, view: View?, position: Int, id: Long) {
+//                val spinnerSelected = position
+//            }
+
+
     }
 }
