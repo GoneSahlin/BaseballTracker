271419bb7dbf213aff8568d7d1a9a359d2344f86
GoneSahlin <zach@sahlins.net>
2022-12-05 02:48:00 -0800

    added PlayerGameStats

---
+++ app/src/main/java/com/example/baseballtracker/PlayerGameStats.kt
@@ -0,0 +1,13 @@
+package com.example.baseballtracker
+
+class PlayerGameStats {
+    var atBats = 0
+    var hits = 0
+    var singles = 0
+    var doubles = 0
+    var triples = 0
+    var homeRuns = 0
+    var rbis = 0
+    var walks = 0
+    var strikeOuts = 0
+}
